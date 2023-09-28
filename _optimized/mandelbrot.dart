2f84056c23fbf80e5f48744ab6ba7d4506d7fc51
Maxim Saplin <smaxmail@gmail.com>
2023-09-28 22:51:25 +0300

    Update mandelbrot.dart

--- _optimized/mandelbrot.dart
+++ _optimized/mandelbrot.dart
@@ -1,3 +1,8 @@
+// V2, No Complex, No func, UintList
+// M1 Pro, sum 78513425
+// dart mandelbrot.dart - Avg: 251.1ms, StdDev: 0.2939%
+// dart compile exe mandelbrot.dart - Avg: 251.3ms, StdDev: 0.3276%
+
 // V1 Original
 // M1 Pro, sum 78513425
 // dart mandelbrot.dart - Avg: 478.0ms, StdDev: 0.4626%
@@ -20,26 +25,41 @@ const double scalex = (max_x - min_x) / width;
 const double scaley = (max_y - min_y) / height;
 const int MAX_ITERS = 256;
 
-int mandelbrot_0(Complex c) {
-  Complex z = c;
+int mandelbrot_0(double cx, double cy) {
+  var zx = cx, zy = cy;
   int nv = 0;
-  for (int i = 1; i < MAX_ITERS; i++) {
-    if (z.abs() > 2) {
+  for (nv; nv < MAX_ITERS - 1; nv++) {
+    final zzx = zx * zx;
+    final zzy = zy * zy;
+    if (zzx + zzy > 4) {
       break;
     }
-    z = z * z + c;
-    nv += 1;
+    double new_zx = zzx - zzy + cx;
+    zy = 2 * zx * zy + cy;
+    zx = new_zx;
   }
   return nv;
 }
 
-Uint32List mandelbrot() {
-  var output = Uint32List(width * height);
+Uint8List mandelbrot() {
+  var output = Uint8List(width * height);
   for (int h = 0; h < height; h++) {
     double cy = min_y + h * scaley;
     for (int w = 0; w < width; w++) {
       double cx = min_x + w * scalex;
-      output[h * width + w] = mandelbrot_0(Complex(cx, cy));
+      var zx = cx, zy = cy;
+      int nv = 0;
+      for (nv; nv < MAX_ITERS - 1; nv++) {
+        final zzx = zx * zx;
+        final zzy = zy * zy;
+        if (zzx + zzy > 4) {
+          break;
+        }
+        double new_zx = zzx - zzy + cx;
+        zy = 2 * zx * zy + cy;
+        zx = new_zx;
+      }
+      output[h * width + w] = nv;
     }
   }
   return output;

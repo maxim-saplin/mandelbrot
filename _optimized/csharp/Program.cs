7b774b992f9552e2d744ff7fbc3b6399d49afdca
Sergei Zinchenko <sergei_zincheko@epam.com>
2023-10-04 03:00:00 +0300

    [feature/optisharp3] avx&avx2

--- _optimized/csharp/Program.cs
+++ _optimized/csharp/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
@@ -21,21 +22,16 @@ namespace ConsoleApp
         static readonly float ScaleY = (MaxY - MinY) / Height;
         static readonly int MaxIters = 256;
         static readonly int NumCpu = Environment.ProcessorCount;
-        static readonly Vector<float> MinYVec = new(MinY);
-        static readonly Vector<float> ScaleXVec = new(ScaleX);
-        static readonly Vector<float> ScaleYVec = new(ScaleY);
-        static readonly Vector<float> FourVec = new(4.0f);
-        private static readonly Vector<float> TwoVec = new(2f);
+        static readonly Vector256<float> MinYVec = Vector256.Create(MinY);
+        static readonly Vector256<float> ScaleXVec = Vector256.Create(ScaleX);
+        static readonly Vector256<float> ScaleYVec = Vector256.Create(ScaleY);
+        static readonly Vector256<uint> IdentVector = Vector256.Create<uint>(1);
+        static readonly Vector256<float> FourVec = Vector256.Create(4.0f);
+        private static readonly Vector256<float> TwoVec = Vector256.Create(2f);
         private static readonly float[] DisplacementArray = { 0, 1, 2, 3, 4, 5, 6, 7 };
-        private static readonly Vector<float> DisplacementVector = new(DisplacementArray);
-        private static readonly int[] Result = new int[Height * Width];
-        private static readonly int Hw = Height * Width;
-
-        private static readonly Vector256<int> InverseMask256 =
-            Vector256.Create(new[] { 7, 6, 5, 4, 3, 2, 1, 0 });
-
-        private static readonly Vector128<int> InverseMask128 =
-            Vector128.Create(new[] { 3, 2, 1, 0 });
+        private static readonly Vector256<float> DisplacementVector = Vector256.Create(DisplacementArray);
+        private static readonly uint[] Result = new uint[Height * Width];
+        private static unsafe volatile uint* _pResult;
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void MandelbrotSimd()
@@ -44,96 +40,95 @@ namespace ConsoleApp
             {
                 MaxDegreeOfParallelism = NumCpu
             };
-            Parallel.For(0, Height / 2 + 1, options, ComplexPlaneSimd);
+            Parallel.For(0, Height / 2   , options, ComplexPlaneSimd);
         }
-        
+
         private static void ComplexPlaneSimd(int h)
         {
             unchecked
             {
                 int offset = h * Width;
-                for (int w = 0; w < Width; w += Vector<float>.Count)
+                int mirrorOffset = (Height - h - 1) * Width;
+                for (int w = 0; w < Width; w += Vector256<float>.Count)
                 {
-                    var cyVec = Vector.Add(MinYVec, Vector.Multiply(new Vector<float>(h), ScaleYVec));
-                    var cxVec = Vector.Add(new Vector<float>(MinX + w * ScaleX),
-                        Vector.Multiply(DisplacementVector, ScaleXVec));
-                    Mandelbrot_0_simd(offset + w, cxVec, cyVec);
+                    var cyVec = Avx.Add(MinYVec, Avx.Multiply(Vector256.Create((float)h), ScaleYVec));
+                    var cxVec = Avx.Add(Vector256.Create(MinX + w * ScaleX),
+                        Avx.Multiply(DisplacementVector, ScaleXVec));
+                    Mandelbrot_0_simd(offset + w, mirrorOffset + w, cxVec, cyVec);
                 }
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void Mandelbrot_0_simd(int offset, Vector<float> cReVec, Vector<float> cImVec)
+        private static unsafe void Mandelbrot_0_simd(int offset, int mirrorOffset, Vector256<float> cReVec, Vector256<float> cImVec)
         {
             unchecked
             {
-                var zReVec = Vector<float>.Zero;
-                var zImVec = Vector<float>.Zero;
-                var nvVec = Vector<int>.Zero;
-                var breakVec = Vector<int>.Zero;
+                var zReVec = Vector256<float>.Zero;
+                var zImVec = Vector256<float>.Zero;
+                var nvVec = Vector256<uint>.Zero;
+                var breakVec = Vector256.Create<uint>(0);
                 for (int i = 1; i < MaxIters; i++)
                 {
-                    var zReNewVec =
-                        Vector.Add(Vector.Subtract(Vector.Multiply(zReVec, zReVec), Vector.Multiply(zImVec, zImVec)),
-                            cReVec);
-                    var zImNewVec = Vector.Add(Vector.Multiply(Vector.Multiply(zReVec, zImVec), TwoVec),
-                        cImVec);
-                    var mag2Vec = Vector.Add(Vector.Multiply(zReNewVec, zReNewVec),
-                        Vector.Multiply(zImNewVec, zImNewVec));
-                    var maskVec = Vector.GreaterThan(mag2Vec, FourVec);
-                    breakVec = Vector.BitwiseOr(breakVec, Vector.Negate(maskVec));
-                    nvVec = Vector.ConditionalSelect(Vector.Equals(breakVec, Vector<int>.Zero),
-                        Vector.Add(nvVec, Vector<int>.One), nvVec);
-                    if (Vector.Dot(breakVec, Vector<int>.One) == Vector<int>.Count)
+                    var zReNewVec = Avx.Add(Avx.Subtract(Avx.Multiply(zReVec, zReVec), Avx.Multiply(zImVec, zImVec)),
+                        cReVec);
+                    var zImNewVec = Avx.Add(Avx.Multiply(Avx.Multiply(zReVec, zImVec), TwoVec), cImVec);
+                    var mag2Vec = Avx.Add(Avx.Multiply(zReNewVec, zReNewVec), Avx.Multiply(zImNewVec, zImNewVec));
+                    var maskVec = Avx2.Add(Avx.CompareLessThan(mag2Vec, FourVec).AsUInt32(), IdentVector);
+                    breakVec = Avx2.Or(maskVec, breakVec);
+                    nvVec = Avx2.Add(nvVec,
+                        Avx2.AndNot(maskVec, IdentVector));
+                    if (Avx.TestZ(Avx2.AndNot(breakVec, IdentVector), IdentVector))
                     {
                         break;
                     }
                     zReVec = zReNewVec;
                     zImVec = zImNewVec;
                 }
-
-                nvVec.CopyTo(Result, offset);
-                switch (Vector<int>.Count)
-                {
-                    case 4:
-                        Vector128.Shuffle(nvVec.AsVector128(), InverseMask128)
-                            .CopyTo(Result, Hw - offset - 4);
-                        break;
-                    case 8:
-                        Vector256.Shuffle(nvVec.AsVector256(), InverseMask256)
-                            .CopyTo(Result, Hw - offset - 8);
-                        break;
-                }
+                Avx.Store(_pResult + offset, nvVec);
+                Avx.Store(_pResult + mirrorOffset , nvVec);
             }
         }
 
-        public static void Main()
+        public static unsafe void Main()
         {
             Console.WriteLine("NumCpu : {0}", NumCpu);
-            Console.WriteLine("IsHardwareAccelerated : {0}", Vector.IsHardwareAccelerated);
-            Console.WriteLine("Vector<float>.Count : {0}", Vector<float>.Count);
-            var measurements = new List<double>();
-            for (int i = -1; i < 10; i++)
+            Console.WriteLine("Avx.IsSupported : {0}", Avx.IsSupported);
+            Console.WriteLine("Avx2.IsSupported : {0}", Avx2.IsSupported);
+            Console.WriteLine("Vector256.IsHardwareAccelerated : {0}", Vector256.IsHardwareAccelerated);
+            fixed (uint* pr = Result)
             {
-                Console.Write(i + 1 + "\t ");
-                Console.Out.Flush();
-                Array.Clear(Result);
-                var stopWatch = new Stopwatch();
-                stopWatch.Start();
-                MandelbrotSimd();
-                stopWatch.Stop();
-                var executionTime = stopWatch.Elapsed;
-                if (i >= 0) {
-                    measurements.Add(executionTime.TotalMilliseconds);
+                _pResult = pr;
+
+                var measurements = new List<double>();
+                for (int i = -1; i < 100; i++)
+                {
+                    Console.Write(i + 1 + "\t ");
+                    Console.Out.Flush();
+                    Array.Clear(Result);
+                    var stopWatch = new Stopwatch();
+                    stopWatch.Start();
+                    MandelbrotSimd();
+                    stopWatch.Stop();
+                    var executionTime = stopWatch.Elapsed;
+                    if (i >= 0)
+                    {
+                        measurements.Add(executionTime.TotalMilliseconds);
+                    }
+
+                    var sum = Result.Aggregate(0u, (s, u) => s + u);
+                    Console.WriteLine("Execution Time:      {0:F2}ms\t  {1}", executionTime.TotalMilliseconds, sum);
                 }
-                var sum = Result.Sum();
-                Console.WriteLine("Execution Time:      {0:F2}ms\t  {1}", executionTime.TotalMilliseconds, sum);
+
+                var average = measurements.Average();
+                var sumOfSquares = measurements.Select(x => Math.Pow(x - average, 2)).Sum();
+                var standardDeviation = Math.Sqrt(sumOfSquares / (measurements.Count - 1)) / average * 100;
+                Console.WriteLine("Avg: {0:F2}ms, StdDev: {1:F2}%", average, standardDeviation);
             }
-            
-            var average = measurements.Average();
-            var sumOfSquares = measurements.Select(x => Math.Pow(x - average, 2)).Sum();
-            var standardDeviation = Math.Sqrt(sumOfSquares / (measurements.Count - 1)) / average * 100;
-            Console.WriteLine("Avg: {0:F2}ms, StdDev: {1:F2}%", average, standardDeviation);
+            string[] resultStrings = Result.Select(x => x.ToString()).ToArray();
+            string resultFile = string.Join(",", resultStrings);
+            string filePath = "output.txt";
+            File.WriteAllText(filePath, resultFile);
         }
     }
 }

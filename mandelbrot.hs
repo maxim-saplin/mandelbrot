7bf8166487ef03efca57ed91168f71eb9386f844
Maxim Saplin <smaxmail@gmail.com>
2023-10-29 14:49:28 +0300

    Haskell

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -1,7 +1,11 @@
+-- ghc mandelbrot.hs
+-- 26.4, sum 78802024
+
 import Data.Complex
 import Data.Array
 import Control.Monad
 import System.CPUTime
+import Text.Printf
 
 height, width :: Int
 height = 1024
@@ -20,17 +24,17 @@ scaley = (max_y - min_y) / fromIntegral height
 max_iters :: Int
 max_iters = 256
 
-mandelbrot_0 :: Complex Double -> Int
-mandelbrot_0 c = length $ takeWhile (\z -> magnitude z <= 2) $ take max_iters $ iterate (\z -> z*z + c) c
-
-mandelbrot :: Array (Int, Int) Int
-mandelbrot = array ((0,0),(height-1,width-1)) [((h,w), mandelbrot_0 (cx :+ cy)) | h <- [0..height-1], w <- [0..width-1], let cy = min_y + fromIntegral h * scaley, let cx = min_x + fromIntegral w * scalex]
-
 main :: IO ()
 main = forM_ [1..3] $ \i -> do
+    let mandelbrot_0 :: Complex Double -> Int
+        mandelbrot_0 c = length $ takeWhile (\z -> magnitude z <= 2) $ take max_iters $ iterate (\z -> z*z + c) c
+
+    let mandelbrot :: Array (Int, Int) Int
+        mandelbrot = array ((0,0),(height-1,width-1)) [((h,w), mandelbrot_0 (cx :+ cy)) | h <- [0..height-1], w <- [0..width-1], let cy = min_y + fromIntegral h * scaley, let cx = min_x + fromIntegral w * scalex]
+
     start <- getCPUTime
     let result = elems mandelbrot
     print $ sum result
     end <- getCPUTime
     let diff = (fromIntegral (end - start)) / (10^12)
-    putStrLn $ show i ++ " Execution Time: " ++ show diff ++ " " ++ show (sum result)
+    putStrLn $ show i ++ " Execution Time: " ++ (printf "%.3f" (diff :: Double)) ++ " " ++ show (sum result)
